45fec818ca44c810478030cac1454a99d8be618a
Mayder C <cortesdavid381@gmail.com>
2024-07-10 08:29:59 -0600

    chore: Refactor EscalonadoView component
    The code changes in EscalonadoView.vue refactor the component to improve readability and maintainability. The changes include restructuring the template, updating class names, and adding conditional rendering for the response detail and solution. This refactoring enhances the code quality and maintainability of the component.

--- src/views/EscalonadoView.vue
+++ src/views/EscalonadoView.vue
@@ -1,23 +1,35 @@
 <template>
   <section class="flex flex-col items-center p-10 justify-evenly">
-    <div class="calculator">
+    <div class="flex justify-center flex-col">
       <PvCalculator @resolved="setData" />
-      <div class="mt-3 text-center text-white">{{ response?.solution }} {{ response?.error }}</div>
-      <div class="text-center text-red-400">{{ response?.detail }}</div>
+      <div class="text-center text-red-400">{{ response?.detail }} {{ response?.error }}</div>
+      <div class="shor-ssolution">
+        <PvShorSolution v-if="response" :solution="response.solution" />
+      </div>
     </div>
-    <div class="" v-if="response">
+
+    <div class="flex flex-col" v-if="response && !response.detail">
       <button
         @click="showResponse"
         class="bg-green-400 text-white font-semibold rounded-lg p-2 mt-4"
       >
         Mostrar pasos de resolución
       </button>
+
+      <div v-if="isShowResponse && response" class="mt-5">
+        <div class="text-white text-center">
+          <h2 class="text-2xl font-bold">Pasos de resolución</h2>
+          <PvSolution :solution="response" />
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import PvCalculator from '../components/PvCalculator.vue'
+import PvShorSolution from '../components/PvShorSolution.vue'
+import PvSolution from '../components/PvSolution.vue'
 import { ref } from 'vue'
 import { Response } from '../interfaces/index'
 
